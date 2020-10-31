052e77aedf9eec2b6c76684f7bc54fa75885fb53
Jan Ruthardt <jan.ruthardt@bcmsolutions.de>
2020-10-31 13:45:29 +0100

    classification of reviews

--- classification.py
+++ classification.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pprint
 from itertools import chain
 from os import listdir
+import csv
 import pdb
 
 from nltk.stem.snowball import EnglishStemmer
@@ -28,16 +29,24 @@ analyzer = TfidfVectorizer().build_analyzer()
 stemmer = EnglishStemmer()
 
 
+def get_df_of_dir(dir_path: str):
+    files = listdir(dir_path)
+    df = pd.DataFrame()
+    for file in files:
+        df_new = pd.read_csv(dir_path + file, engine='python',
+                             header=None, encoding='utf-8', error_bad_lines=False, quoting=csv.QUOTE_NONE, delimiter="§")
+        df = pd.concat([df, df_new], axis=0)
+    return df
+
+
 def read_trip_reviews():
     venice_dir = "Al_Ponte_Antico_Hotel/en/"
-    venice_files = listdir(venice_dir)
-    df_venice = pd.DataFrame()
-    for file in venice_files:
-        print(file)
-        df = pd.read_csv(venice_dir + file, engine='python')
-        df_venice = pd.concat([df_venice, df], axis=1)
-    pdb.set_trace()
-    df_stuttgart = pd.read_csv("Mövenpick_Hotel_Stuttgart_Airport")
+    stuttgart_dir = "Mövenpick_Hotel_Stuttgart_Airport/en/"
+    df_venice = get_df_of_dir(venice_dir)
+    df_venice['location'] = "venice"
+    df_stuttgart = get_df_of_dir(stuttgart_dir)
+    df_stuttgart['location'] = "stuttgart"
+    return pd.concat([df_venice, df_stuttgart], ignore_index=True)
 
 
 def stemmed_words(doc):
@@ -70,15 +79,27 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 #### Modelling ####
-# clf = ComplementNB()
+#clf = ComplementNB()
 clf = LinearSVC()
 model = clf.fit(X_train, y_train)
 prediction = model.predict(X_test)
 
 
 #### Testing Reviews ####
-read_trip_reviews()
+df_reviews = read_trip_reviews()
+df_venice = df_reviews[df_reviews['location'] == 'venice']
+df_stuttgart = df_reviews[df_reviews['location'] == 'stuttgart']
+review_vector_venice = tfidf_vectorizer.transform(df_venice[0].to_numpy())
+review_vector_stuttgart = tfidf_vectorizer.transform(
+    df_stuttgart[0].to_numpy())
 
-report = classification_report(y_test, prediction, output_dict=True)
-score = model.score(X_test, y_test)
-pprint.pprint(report)
+fake_percentage_venice = model.predict(review_vector_venice).mean() * 100
+fake_percentage_stuttgart = model.predict(review_vector_venice).mean()*100
+print("Fake reviews in Venice: " + str(fake_percentage_venice))
+print("Fake reviews in Stuttgart: " + str(fake_percentage_stuttgart))
+# pdb.set_trace()
+
+
+#report = classification_report(y_test, prediction, output_dict=True)
+#score = model.score(X_test, y_test)
+# pprint.pprint(report)
