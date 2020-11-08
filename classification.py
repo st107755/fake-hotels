ff683b0eace148100591567efa565b9446c5f15c
Jan Ruthardt <jan.ruthardt@bcmsolutions.de>
2020-11-08 16:12:48 +0100

    upsampling to get better recall

--- classification.py
+++ classification.py
@@ -5,6 +5,7 @@ from itertools import chain
 from os import listdir
 import csv
 import pdb
+import nltk
 
 from nltk.stem.snowball import EnglishStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,6 +13,12 @@ from stop_words import get_stop_words
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 from sklearn.naive_bayes import ComplementNB, GaussianNB, MultinomialNB
+from autosklearn.experimental.askl2 import AutoSklearn2Classifier
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.pipeline import Pipeline
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
 
 from sklearn.metrics import (
     average_precision_score,
@@ -25,8 +32,19 @@ from sklearn.model_selection import (
     train_test_split,
 )
 
-analyzer = TfidfVectorizer().build_analyzer()
-stemmer = EnglishStemmer()
+
+class LemmaTokenizer:
+    ignore_tokens = [',', '.', ';', ':', '"', '``', "''", '`']
+
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+
+    def __call__(self, doc):
+        return [self.wnl.lemmatize(t) for t in word_tokenize(doc) if t not in self.ignore_tokens]
+
+
+tokenizer = LemmaTokenizer()
+token_stop = tokenizer(' '.join(get_stop_words("en")))
 
 
 def get_df_of_dir(dir_path: str):
@@ -49,23 +67,22 @@ def read_trip_reviews():
     return pd.concat([df_venice, df_stuttgart], ignore_index=True)
 
 
-def stemmed_words(doc):
-    return (stemmer.stem(w) for w in analyzer(doc))
-
-
 df = pd.read_csv("fakenew.csv",  delimiter=";")
-
 features = df['Review'].to_numpy()
 label = df['Fake1'].to_numpy()
 
 ### Counter Vectorize / Stop Words / Stemming / Tfidf ###
-optimal_args = dict(stop_words=get_stop_words(
-    "en"), analyzer=stemmed_words,  min_df=2)
+args = dict(stop_words=token_stop,
+            ngram_range=(1, 5),
+            strip_accents="ascii",
+            min_df=2,
+            tokenizer=LemmaTokenizer(),
+            )
 
-tfidf_vectorizer = TfidfVectorizer(optimal_args)
+tfidf_vectorizer = TfidfVectorizer(args)
 tfidf_vector = tfidf_vectorizer.fit_transform(features)
 
-
+#### Train Test Split ####
 random_seed = np.random.randint(1, len(label))
 X_train, X_test, y_train, y_test = train_test_split(
     tfidf_vector.toarray(),
@@ -76,11 +93,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=label,
 )
 
+##### Over and undersampling #####
+oversample = SMOTE()
+X, y = oversample.fit_resample(X_train, y_train)
+
 #### Modelling ####
 clf = LinearSVC()
-model = clf.fit(X_train, y_train)
+model = clf.fit(X, y)
 prediction = model.predict(X_test)
-
 
 #### Testing Reviews ####
 df_reviews = read_trip_reviews()
@@ -92,11 +112,9 @@ review_vector_stuttgart = tfidf_vectorizer.transform(
 
 fake_percentage_venice = model.predict(review_vector_venice).mean() * 100
 fake_percentage_stuttgart = model.predict(review_vector_stuttgart).mean()*100
-print("Fake reviews in Venedik (in Prozent): " + str(fake_percentage_venice))
-print("Fake reviews in Stuttgart (in Prozent): " + str(fake_percentage_stuttgart))
-# pdb.set_trace()
+#print("Fake reviews in Venedik (in Prozent): " + str(fake_percentage_venice))
+#print("Fake reviews in Stuttgart (in Prozent): " + str(fake_percentage_stuttgart))
 
 
-#report = classification_report(y_test, prediction, output_dict=True)
-#score = model.score(X_test, y_test)
-# pprint.pprint(report)
+report = classification_report(y_test, prediction, output_dict=True)
+pprint.pprint(report)
