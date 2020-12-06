9c9446ba3ef54dcae5ae8e0659996dc18c84e8a7
jan ruthardt <jan.ruthardt@markju.de>
2020-12-06 21:17:31 +0100

    best working modell

--- classification.py
+++ classification.py
@@ -87,16 +87,15 @@ def transver_undersamplingrate(up_rate,down_rate,invert=False):
     return down_rate * inverse_up_rate + up_rate
 
 
-def classification_run(x):
+def classification_run():
+    ### Load Learning Data ###
     df = pd.read_csv("fakenew.csv",  delimiter=";")
     features = df['Review'].to_numpy()
     label = df['Fake1'].to_numpy()
-    print("mean between classes" + str(df.Fake1.mean()))
-    #pdb.set_trace()
 
     ### Counter Vectorize / Stop Words / Stemming / Tfidf ###
     args = dict(stop_words=token_stop,
-                ngram_range=(1, 5),
+                ngram_range=(1, 3),
                 strip_accents="ascii",
                 min_df=2,
                 tokenizer=LemmaTokenizer(),
@@ -105,54 +104,37 @@ def classification_run(x):
     tfidf_vectorizer = TfidfVectorizer(args)
     tfidf_vector = tfidf_vectorizer.fit_transform(features)
     
-    X_train,X_test,y_train,y_test = train_test_split(tfidf_vector, label,test_size=0.2,random_state=42)
+    X_train,X_test,y_train,y_test = train_test_split(tfidf_vector, label,test_size=0.3,random_state=42)
 
     ##### Over and undersampling #####
-    over_sampling_rate = x[0]
-    under_sampling_rate = x[1]
-    '''
-    over_sampling_boundary = 1
-    over_sampling_rate = x[0]
-    if over_sampling_rate < 0:
-        over_sampling_rate=0.13290058079945336
-    elif over_sampling_rate > over_sampling_boundary:
-        over_sampling_rate=over_sampling_boundary
-    under_sampling_rate = x[1]
-    if under_sampling_rate < 0:
-        under_sampling_rate=0
-    elif under_sampling_rate > 1:
-        under_sampling_rate=1
-    '''
-    #pdb.set_trace()
-
-    #if over_sampling_rate > under_sampling_rate:
-    #    under_sampling_rate = over_sampling_rate 
-    under_sampling_rate = transver_undersamplingrate(over_sampling_rate,under_sampling_rate)
-    print("Undersampling rate: " + str(under_sampling_rate))    
-    print("Oversampling rate: " +str(over_sampling_rate))
-    over = SMOTE(sampling_strategy=over_sampling_rate,n_jobs=4)
-    under = RandomUnderSampler(sampling_strategy=under_sampling_rate)
+    over = SVMSMOTE(n_jobs=4)
+    under = RandomUnderSampler()
     steps = [('o', over),('u', under)]
     pipeline = Pipeline(steps=steps)
     X, y = pipeline.fit_resample(X_train,y_train)
-    #X, y = tfidf_vector,label
     
     #### Modelling ####
     f1 = make_scorer(f1_score, average='macro')
-    # parameters = {"C": [0.001, 0.01, 0.1, 1.0, 10.0,
-    #                     20.0, 30.0], "loss": ["hinge", "squared_hinge"],
-    #               "tol": [1e-1, 1e-3, 1e-6]}
-    parameters ={}
+    parameters = {"C": [0.001, 0.01, 0.1, 1.0, 10.0,
+                         20.0, 30.0], "loss": ["hinge", "squared_hinge"],
+                   "tol": [1e-1, 1e-3, 1e-6]}
     model = GridSearchCV(
-        LinearSVC(max_iter=20000), param_grid=parameters, n_jobs=-1, verbose=True, scoring=f1,cv=10
+         LinearSVC(max_iter=20000), param_grid=parameters, n_jobs=-1, verbose=True, scoring=f1,cv=10
     )
-    #model = LinearSVC()
-    # grid_model = SGDClassifier()
-    # clf = KNeighborsClassifier()
-    # grid_model = CatBoostClassifier()
+    #model = 
+    #model = LinearSVC(n_jobs=-1)
+    #grid_model = SGDClassifier()
+    #model = KNeighborsClassifier()
+    #model = CatBoostClassifier()
     model.fit(X, y)
 
+    #### Model Testing ####
+    pred = model.predict(X_test)
+    f1 = f1_score(y_test, pred, average='macro')
+    print("F1 Score: " + str(f1))
+
     #### Testing Reviews ####
+
     df_reviews = read_trip_reviews()
     df_venice = df_reviews[df_reviews['location'] == 'venice']
     df_stuttgart = df_reviews[df_reviews['location'] == 'stuttgart']
@@ -164,20 +146,11 @@ def classification_run(x):
         DenseTransformer().fit_transform(review_vector_venice)).mean() * 100
     fake_percentage_stuttgart = model.predict(
         DenseTransformer().fit_transform(review_vector_stuttgart)).mean()*100
-    #print("Fake reviews in Venedik (in Prozent): " + str(fake_percentage_venice))
-    # print("Fake reviews in Stuttgart (in Prozent): " +
-    #      str(fake_percentage_stuttgart))
-    #pprint.pprint("weighted f1 score: " + str(grid_model.best_score_))
-    #return {"fake_stuttgart": fake_percentage_stuttgart, "fake_venice": fake_percentage_venice, "f1": model.best_score_}
-    #print("F1 Score: " + str(model.best_score_))
-    pred = model.predict(X_test)
-    f1 = f1_score(y_test, pred, average='weighted')
-    print("F1 Score: " + str(f1))
-    return 1 - f1
 
-bounds = [(0.2, 1), (0, 1)]
-res = minimize(classification_run, bounds)
-print(res)
+    print("Fake reviews in Venedik (in Prozent): " + str(fake_percentage_venice))
+    print("Fake reviews in Stuttgart (in Prozent): " +
+         str(fake_percentage_stuttgart))
+    return {"fake_stuttgart": fake_percentage_stuttgart, "fake_venice": fake_percentage_venice, "f1": f1}
 
-#print(classification_run(x0))
+print(classification_run())
 
